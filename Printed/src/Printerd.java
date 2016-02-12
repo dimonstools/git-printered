ceb1786adb2c2a132e4cc53773318c3bcaf51533
Дмитро Вараніцький < >
2016-02-12 13:54:38 +0200

    Тестовое задание. Вывод текста на Java и заливание на github

---
+++ Printed/src/Printerd.java
@@ -0,0 +1,16 @@
+
+public class Printerd {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 47;
+		String s = "Äðàñòº";
+		String s1=(s+a+b);
+		Secondary (s1);
+	}
+
+	public static void Secondary (String s){
+		System.out.println(s);
+		System.out.println(s);
+	}
+}
